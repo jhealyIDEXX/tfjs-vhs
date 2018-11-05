69a9c1685e77b435d4e35cc5cdc3db884aa29a76
Joe Healy <joe-healy@idexx.com>
2018-11-05 14:48:52 -0500

    write point labels

---
+++ static/point_labels.js
@@ -0,0 +1,20 @@
+export const POINT_LABELS = {
+	0: 'Apex x',
+	1: 'Apex y',
+	2: 'TC x',
+	3: 'TC y',
+	4: 'M1 x',
+	5: 'M1 y',
+	6: 'M2 x',
+	7: 'M2 y',
+	8: 'T4 x',
+	9: 'T4 y',
+	10: 'T5 x',
+	11: 'T5 y',
+	12: 'T6 x',
+	13: 'T6 y',
+	14: 'T7 x',
+	15: 'T7 y',
+	16: 'T8 x',
+	17: 'T8 y'  	
+};
